0fcfacf6f1bc0e7909c48b213df52312815d6267
ktanidis <60473500+ktanidis@users.noreply.github.com>
2020-07-15 18:57:02 +0200

    Add files via upload

---
+++ config/subdirs.mk
@@ -0,0 +1,18 @@
+define process_subdirs
+	@for d in $(SUBDIRS); \
+	do \
+	  (cd $$d && $(MAKE) $@) || exit $$?; \
+	done
+endef
+
+.PHONY: all clean test
+
+all::
+	$(process_subdirs)
+
+test:
+	$(process_subdirs)
+
+clean:
+	$(process_subdirs)
+
